8045aa53943d4038361963088b6cbc8d19f07e84
Abdessittir <harkati.web.dev@gmail.com>
2023-05-28 10:21:04 +0100

    Remove tests for now

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const app = express();
+const app = express()
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
